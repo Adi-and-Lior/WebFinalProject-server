326b2fae206381dc4437f57256c32362ce8e9ad4
LiorBoaron <lior.boaron8@gmail.com>
2025-07-28 16:32:53 +0300

    fix: change in css to diffrent files

--- routes/auth.js
+++ routes/auth.js
@@ -37,27 +37,4 @@ router.post('/login', async (req, res) => {
   }
 });
 
-/* ---------- Handles new user registration requests ---------- */
-router.post('/register', async (req, res) => {
-  const { username, password, userType, city } = req.body;
-  try {
-    if (await User.findOne({ username, userType })) {
-      return res.status(409).json({ error: 'משתמש עם שם משתמש וסוג זה כבר קיים.' });
-    }
-    const hashed = await bcrypt.hash(password, 10);
-    const newUser = await new User({ username, password: hashed, userType, city }).save();
-    res.status(201).json({
-      user: {
-        username: newUser.username,
-        userType: newUser.userType,
-        userId: newUser._id.toString(),
-        city: newUser.city
-      }
-    });
-  } catch (err) {
-    console.error('Error registering new user:', err.message);
-    res.status(500).json({ error: 'שגיאה בעת הרשמת משתמש חדש.' });
-  }
-});
-
 module.exports = router;
